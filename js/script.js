800b53cbf5628c6df9917a5a2a3c08537e72725e
Michal Dokupil <michal@dokupil.pl>
2017-12-27 14:13:26 +0100

    change var names

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var a = prompt('Podaj zmienna \'A\'');
 var b = prompt('Podaj zmienna \'B\'');
-var dodatni= 'dodatni';
-var ujemny='kwadrat różnicy jest zawsze dodatni';
+var morethan0= 'dodatni';
+var lessthan0='kwadrat różnicy jest zawsze dodatni';
 
     calc = (a*a) - (2*a*b) + (b*b);
 
 if (calc < 0 ) {
-    document.getElementById("status").innerHTML = ujemny;
+    document.getElementById("status").innerHTML = lessthan0;
 }else if ( calc > 0){
-    document.getElementById("status").innerHTML = dodatni;
+    document.getElementById("status").innerHTML = morethan0;
 }else {
     alert('Wynik jest równy zero');
 }
